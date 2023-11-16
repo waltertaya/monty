9bed2b81b3fc710e56fa4480752b9caf9afa4e09
Baruk1-netizen <134915896+Baruk1-netizen@users.noreply.github.com>
2023-11-16 20:29:44 +0300

    push pall

---
+++ push-pall.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_SIZE 100
+
+int stack[STACK_SIZE];
+int top = -1;
+void push(int value) {
+    if (top == STACK_SIZE - 1) {
+        fprintf(stderr, "Error: Stack overflow\n");
+        exit(EXIT_FAILURE);
+    } else {
+        top++;
+        stack[top] = value;
+    }
+}
+void pall() {
+    int i;
+    for (i = top; i >= 0; i--) {
+        printf("%d\n", stack[i]);
+    }
+}
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    pall();
+
+    return 0;
+}
