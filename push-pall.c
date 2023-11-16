8213d46dde9f277508ac96ee2d5647953ae2394a
Baruk1-netizen <134915896+Baruk1-netizen@users.noreply.github.com>
2023-11-16 21:57:21 +0300

    Update push-pall.c

--- push-pall.c
+++ push-pall.c
@@ -1,30 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "monty.h"
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, j = 0, flag = 0;
 
-#define STACK_SIZE 100
-
-int stack[STACK_SIZE];
-int top = -1;
-void push(int value) {
-    if (top == STACK_SIZE - 1) {
-        fprintf(stderr, "Error: Stack overflow\n");
-        exit(EXIT_FAILURE);
-    } else {
-        top++;
-        stack[top] = value;
-    }
-}
-void pall() {
-    int i;
-    for (i = top; i >= 0; i--) {
-        printf("%d\n", stack[i]);
-    }
-}
-int main() {
-    push(10);
-    push(20);
-    push(30);
-    pall();
-
-    return 0;
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
